cc3a0ec607ddf3f660c4c1106e09d30ee62a03fb
Edem Dumenu <ehdemdume@gmail.com>
2018-04-22 16:22:56 -0400

    A few changes after I've finished commenting

--- config/database.js
+++ config/database.js
@@ -3,5 +3,7 @@
 //Description: This page sets up the configuration for the mongodb connection
 
 module.exports = {
-    mongoDbUrl: 'mongodb://localhost:27017/cms'    //Url for the database
+    // mongoDbUrl: 'mongodb://localhost:27017/cms'    //Url for the database
+    mongoDbUrl: 'mongodb://cms-user:123@ds117605.mlab.com:17605/cms'    //Url for the database
+
 };
